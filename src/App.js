f05ce868eefbe8066800bce38195d7f85f5bd16c
Wagar1 <vuqar.eyvazov.1997@gmail.com>
2022-01-24 10:07:06 +0400

    Added autocomplete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
-  return (
-    <div className="App">
-      Search Wiki
-    </div>
-  );
+import { useState } from 'react';
+import ReactAutocomplete from 'react-autocomplete';
+
+const App = () => {
+    const [value, setValue] = useState();
+    return (
+        <ReactAutocomplete
+            items={[
+                { id: 'foo', label: 'foo' },
+                { id: 'bar', label: 'bar' },
+                { id: 'baz', label: 'baz' },
+            ]}
+            shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+            getItemValue={item => item.label}
+            renderItem={(item, highlighted) =>
+                <div
+                    key={item.id}
+                    style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                >
+                    {item.label}
+                </div>
+            }
+            value={value}
+            onChange={e => setValue( e.target.value )}
+            onSelect={value => setValue(value)}
+        />
+    )
 }
 
 export default App;
